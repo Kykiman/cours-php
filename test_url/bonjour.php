f8f8b6ec012f3b004c3983d57831de36c17c9ef7
Kykiman <kykiman2000@gmail.com>
2018-03-08 09:23:37 +0100

    Changement des prénoms

---
+++ test_url/bonjour.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+<title>titre</title>
+<meta charset="utf-8">
+    </head>
+    <body>
+
+       <p>Bonjour <?php echo $_GET['prenom']; ?> !</p>
+    </body>
+</html>
