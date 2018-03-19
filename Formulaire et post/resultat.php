8893351dd0a9f2073ccb11a3aaf18dbb71518f12
Kykiman <kykiman2000@gmail.com>
2018-03-19 13:37:56 +0100

    1

---
+++ Formulaire et post/resultat.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+<?php
+$semaineDeTravail = $_POST["semaineDeTravail"];
+$age = $_POST["age"];
+$color = $_POST["color"];
+$genre =  $_POST["genre"];
+$condition =  $_POST["condition"];
+?>
+<p>
+    <php? echo ($semaineDeTravail . $age $color . $genre . $condition;)
+   ?> </p>
+</body>
+</html>
