37d504e76a37a96779365066f1af7e1f87519ff1
Kykiman <kykiman2000@gmail.com>
2018-03-19 15:04:57 +0100

    fin du cours

--- Formulaire et post/resultat.php
+++ Formulaire et post/resultat.php
@@ -1,19 +1,62 @@
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <title>Document</title>
 </head>
+
 <body>
-<?php
+    <?php
+$pseudo = $_POST["pseudo"];
 $semaineDeTravail = $_POST["semaineDeTravail"];
 $age = $_POST["age"];
 $color = $_POST["color"];
 $genre =  $_POST["genre"];
-$condition =  $_POST["condition"];
+$condition = isset($_POST["condition"]);
 ?>
-<p>
-    <php? echo ($semaineDeTravail . $age $color . $genre . $condition;)
-   ?> </p>
+
+    <?php if($condition===true) { ?>
+               
+        <h1>Votre nom de G@m3r :</h1>
+        <p><?= $pseudo ?></p>
+
+        <h1>Etes-vous pour la semaine de 20 heures de travail ?</h1>
+        <?= $semaineDeTravail;?>
+
+        <h1>Tu es :</h1>
+        <?=  $genre ;?>
+
+        <h1>les bougies du capitaine</h1>         
+            <?php  echo '<p>';
+                for ( $i=0 ; $i<$age ; $i++ ) {
+                    echo "I";
+                    }
+            echo '</p>'; ?>
+
+        <h1>Votre couleur :</h1>
+        <div class="couleur"></div>  
+        <img src=<?php 
+                    if($genre===homme){
+                        echo '"homme.jpg"';
+                    }
+                    elseif($genre===femme){
+                        echo '"femme.jpg"';
+                    }
+                    else{
+                        echo '"caillou.jpg"';
+             }?>  alt="image de votre genre">
+     <?php   }
+            else {
+                echo "<h2>Tu n'as pas accepté les conditions</h2>";
+            }
+            ?>
 </body>
+<style>
+   div.couleur{
+    background-color: <?= $color ?>;
+    height: 50px; 
+    width: 150px;
+   } 
+</style>
 </html>
